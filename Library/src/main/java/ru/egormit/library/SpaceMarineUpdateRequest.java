1da7baf8580ca5e13188415d924fede778c12030
EgorMIt <egor.mitrofanov@hotmail.com>
2022-10-30 13:51:22 -0700

    update request

---
+++ Library/src/main/java/ru/egormit/library/SpaceMarineUpdateRequest.java
@@ -0,0 +1,67 @@
+package ru.egormit.library;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.egormit.library.enums.AstartesCategory;
+import ru.egormit.library.enums.MeleeWeapon;
+import ru.egormit.library.enums.Weapon;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Модель Space Marine.
+ *
+ * @author Egor Mitrofanov.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+public class SpaceMarineUpdateRequest {
+
+    /**
+     * Идентификатор.
+     */
+    private Long id;
+
+    /**
+     * Имя.
+     */
+    private String name;
+
+    /**
+     * Координаты.
+     */
+    private Coordinates coordinates;
+
+    /**
+     * Дата создания.
+     */
+    private ZonedDateTime creationDate;
+
+    /**
+     * Количество здоровья.
+     */
+    private Long health;
+
+    /**
+     * Класс персонажа.
+     */
+    private AstartesCategory category;
+
+    /**
+     * Тип оружия.
+     */
+    private Weapon weaponType;
+
+    /**
+     * Тип оружия ближнего боя.
+     */
+    private MeleeWeapon meleeWeapon;
+
+    /**
+     * Корабль.
+     */
+    private StarShipDto starShip;
+
+}
